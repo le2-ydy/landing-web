e0e06206af52818f5fe88f4ad05a0885d4c349a1
蘇 <su@le2.ltd>
2026-07-28 22:01:08 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@ import { createApp } from "vue";
 import { createHead } from "@unhead/vue/client";
 import App from "./App.vue";
 import router from "./router";
-import "./styles.css";
+import "./styles.scss";
 
 createApp(App).use(router).use(createHead()).mount("#app");
